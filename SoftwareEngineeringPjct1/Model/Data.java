d6b2a0714de8a2574322d05edbcfc3d9d869e106
Bumilibb <raidam1@newpaltz.edu>
2024-02-04 17:58:52 -0500

    many changes

---
+++ SoftwareEngineeringPjct1/Model/Data.java
@@ -0,0 +1,9 @@
+public static class Data {
+    private int number;
+    private List<Integer> primeFactors;
+    private List<Integer> divisors;
+    private String visualization;
+    
+    // Getters and Setters
+}
+}
---
+++ SoftwareEngineeringPjct1/Model/NumOutputData.java
@@ -0,0 +1,10 @@
+public class NumOutputData {
+    private String outputType;
+    private Data data;
+    
+    public OutputData(String outputType, Data data) {
+        this.outputType = outputType;
+        this.data = data;
+    }
+    
+    // Getters and Setters
