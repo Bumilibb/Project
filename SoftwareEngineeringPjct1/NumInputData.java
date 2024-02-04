fe1e7b349f577ef677348b1dacf34ab9fdc24d03
Bumilibb <raidam1@newpaltz.edu>
2024-02-04 17:45:23 -0500

    NumInputData java added

---
+++ SoftwareEngineeringPjct1/NumInputData.java
@@ -0,0 +1,10 @@
+public class NumInputData {
+    public class NumberInputDto {
+        private String inputType;
+        private String data;
+        private String outputType;
+    
+        // Constructor, Getters, and Setters
+    }
+    
+}
