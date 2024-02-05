ebfb0e907da902ca650192aec6b9eb7e572631fc
Bumilibb <raidam1@newpaltz.edu>
2024-02-05 02:52:09 -0500

    deleted

--- SoftwareEngineeringPjct1/Model/Data.java
+++
@@ -1,8 +0,0 @@
-public static class Data {
-    private int number;
-    private List<Integer> primeFactors;
-    private List<Integer> divisors;
-    private String visualization;
-    
-    // Getters and Setters
-}
